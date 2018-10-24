ad6596d4b5e5e0efb92c5b8d26feef86fc4b915b
sajidrahman <sajidrahman25@gmail.com>
2018-10-24 12:52:27 -0400

    /demoalbum/get added

--- music/src/main/java/com/musicApp/rest/AlbumController.java
+++ music/src/main/java/com/musicApp/rest/AlbumController.java
@@ -4,12 +4,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.*;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport; 
 import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.Produces;
 import java.util.Collection;
@@ -46,6 +46,25 @@ public class AlbumController extends HttpServlet  {
 		return Response.status(200).entity(output).build();
 	}
 
+	@GET
+	@Path("/demoalbum/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Album getAlbum() {
+
+		Album demoAlbum = new Album(42, "Summer of 69");
+
+        Track track = new Track();
+        track.setId(1);
+        track.setTitle("Wind of Change");
+        track.setAlbumId(42);
+
+        ArrayList<Track> tracksList = new ArrayList<Track>();
+        tracksList.add(track);
+
+        demoAlbum.setTracks(tracksList);
+
+		return demoAlbum;
+	}
 
 	@GET
 	@Produces("text/plain")
